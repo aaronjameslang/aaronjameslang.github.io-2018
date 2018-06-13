eb7c9241c597b1ed1c5ef4ea603f59803607501b
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-06-13 20:36:18 +1200

    Don't deploy whole project to s3

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ prettier '**/*.{js,json,ts}' --write
 tslint --project . --fix
 parcel build src/index.html --out-dir dist/parcel
 sed -i- 's/="\//="/g' dist/parcel/index.html
-inline-assets dist/parcel/index.html dist/index.html
-cp dist/index.html ./
+mkdir -p dist/s3
+inline-assets dist/parcel/index.html dist/s3/index.html
+cp dist/s3/* ./
 test -z ${CI-} || git diff --exit-code
